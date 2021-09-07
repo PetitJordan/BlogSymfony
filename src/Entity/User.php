fa11f63d745c022a0a57897fd4d75e685d61dcbe
PetitJordan <jordan.petit.pro@gmail.com>
2021-09-07 17:54:58 +0200

    Part4 SymfonyBlog

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,13 +2,20 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ *  fields= {"email"},
+ *  message= "L'email que vous avez entré est déjà utilisé !"
+ * )
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -19,6 +26,7 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
      */
     private $email;
 
@@ -29,9 +37,17 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *      min = 8,
+     *      minMessage = "Le mot de passe doit faire au moins {{ limit }} caractères")
      */
     private $password;
 
+    /**
+     * @Assert\EqualTo(
+     *      propertyPath="password",
+     *      message="Vous n'avez pas tapé le même mot de passe")
+     */
     public $confirm_password;
 
     public function getId(): ?int
@@ -73,5 +89,14 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    
+    public function eraseCredentials(){}
+
+    public function getSalt(){}    
+  
+    public function getRoles(){
+        return ['ROLE_USER'];
     }
 }
